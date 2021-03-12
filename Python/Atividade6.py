1f4e9ee392cb120acd492249b74ae9a3a68867c6
Erick Brito <erick.novais_bn@hotmail.com>
2021-03-12 11:14:39 -0300

    Python Learnings

--- Python/Atividade6.py
+++ Python/Atividade6.py
@@ -4,16 +4,17 @@ imc = float(peso / (altura*altura))
 if(imc <= 18.5):
     print("Abaixo do peso")
 else: 
-    if(imc >=18.5 and imc <= 24.9):
+    if(imc >=18.5 and imc <25):
         print("Peso Normal")
     else: 
-        if(imc >= 25 and imc <=29.9):
+        if(imc >= 25 and imc <30):
             print("Sobrepeso")
         else:
-            if(imc >= 30 and imc <= 34.9):
+            if(imc >= 30 and imc <35):
                 print("Obesidade Nivel 1")
             else:
-                if(imc >= 35 and imc <=39.9):
+                if(imc >= 35 and imc <40):
                     print("Obesidade Nivel 2")
                 else:
-                    print("Obesidade Nivel 3")
+                    if(imc > 40):
+                        print("Obesidade Nivel 3")
